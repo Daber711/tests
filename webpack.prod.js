8f97acd4d034a89a791f2d95743b28a23c1df228
Alexey Sokolov <dabero7111@gmail.com>
2025-02-03 19:15:50 +0300

    test4

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,14 @@
-const { merge } = require("webpack-merge");
-const common = require("./webpack.common");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require('terser-webpack-plugin');
+const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin');
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common');
 
 module.exports = merge(common, {
-  mode: "production",
+  mode: 'production',
   optimization: {
-    minimizer: [new CssMinimizerPlugin()],
+    minimizer: [
+      new TerserPlugin({}),
+      new CssMinimizerWebpackPlugin({ test: /\.foo\.css$/i }),
+    ],
   },
 });
